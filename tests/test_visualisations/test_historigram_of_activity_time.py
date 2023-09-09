0cd2fba1eeb290d129f1ddab0ee4d16514d304e5
John Scolaro <johnscolaro95@gmail.com>
2023-09-09 12:36:35 +1000

    Histogram of activity start times

---
+++ tests/test_visualisations/test_historigram_of_activity_time.py
@@ -0,0 +1,13 @@
+import pytest
+
+from active_statistics.exceptions import UserVisibleException
+from active_statistics.visualisations.histogram_of_activity_time import plot
+
+
+def test_histogram_of_activity_time(some_basic_runs_and_rides) -> None:
+    plot(some_basic_runs_and_rides)
+
+
+def test_no_data(no_activities_at_all) -> None:
+    with pytest.raises(UserVisibleException):
+        plot(no_activities_at_all)
