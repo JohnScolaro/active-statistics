9d709c65bd77f988e6de77703e48956bbef044c6
John Scolaro <johnscolaro95@gmail.com>
2023-09-29 14:51:16 +1000

    Fix issue in pace timeline with no moving time
    Sometimes the "moving time" of an activity is broken in Strava.
    Typically for very old Garmin uploads, this field is either zero or way
    too large. If it is zero, when we resample to create the moving average
    lines, we can divide by zero, and get extra NaN's and then filter them
    out. Very niche issue, but this takes care of it + adds a test to make
    sure we don't regress.

--- tests/test_visualisations/test_pace_timeline.py
+++ tests/test_visualisations/test_pace_timeline.py
@@ -1,7 +1,11 @@
+import datetime as dt
+
 import pytest
+from stravalib.unithelper import Quantity
 
 from active_statistics.exceptions import UserVisibleException
 from active_statistics.statistics.plots.pace_timeline import plot
+from tests.factories.activity_factories import ActivityFactory
 
 
 def test_pace_timeline(some_basic_runs_and_rides) -> None:
@@ -11,3 +15,20 @@ def test_pace_timeline(some_basic_runs_and_rides) -> None:
 def test_no_data(no_activities_at_all) -> None:
     with pytest.raises(UserVisibleException):
         plot(no_activities_at_all)
+
+
+def test_first_activities_have_no_average_speeds() -> None:
+    activities = [
+        ActivityFactory(
+            type="Run",
+            start_date_local=dt.datetime(2015, 1, 1, 6),
+            average_speed=Quantity(0.0, "m/s"),
+            moving_time=0,
+        ),
+        ActivityFactory(
+            type="Run",
+            start_date_local=dt.datetime(2020, 1, 1, 6),
+            average_speed=Quantity(10, "m/s"),
+        ),
+    ]
+    plot(_ for _ in activities)
