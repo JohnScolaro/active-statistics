6334e2fa2b8453b99f722c6a1bccdfab1daafc56
John Scolaro <JohnScolaro@users.noreply.github.com>
2023-09-29 14:53:55 +1000

    Summary trivia works if you have no activities (#21)

--- tests/test_trivia/test_summary_trivia.py
+++ tests/test_trivia/test_summary_trivia.py
@@ -1,9 +1,17 @@
 from active_statistics.statistics.trivia.summary_trivia import general_trivia
 
 
-def test_general_trivia(some_basic_runs_and_rides) -> None:
+def test_summary_trivia(some_basic_runs_and_rides) -> None:
     """
     A basic test to make sure that if we pump some random data through the
     processor, nothing breaks.
     """
     general_trivia.get_data(some_basic_runs_and_rides)
+
+
+def test_summary_trivia_no_activities() -> None:
+    """
+    A basic test to make sure that if we pump some random data through the
+    processor, nothing breaks.
+    """
+    general_trivia.get_data(_ for _ in [])
