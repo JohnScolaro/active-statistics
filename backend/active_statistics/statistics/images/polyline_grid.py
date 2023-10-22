60933a1d99f79e7bcb852ed5e9fd6d395a22237d
John Scolaro <johnscolaro95@gmail.com>
2023-10-22 21:05:36 +1000

    Polyline images being generated

---
+++ backend/active_statistics/statistics/images/polyline_grid.py
@@ -0,0 +1,254 @@
+import dataclasses
+import datetime as dt
+import math
+from typing import Iterator
+
+import polyline
+from PIL import Image
+from PIL import Image as img
+from PIL import ImageDraw
+from PIL.Image import Image
+
+Polyline = list[tuple[float, float]]
+
+
+def create_image(
+    encoded_polylines: Iterator[str],
+    polyline_fraction: float = 1.0,
+    normalise_activity_time: bool = True,
+    proposed_image_size: int = 2000,
+    border_size_px: int = 1,
+    line_thickness: int = 2,
+) -> Image | None:
+    """
+    Creates and returns a PIL image object with all polylines displayed in a grid.
+
+    max_segments_fraction: This is a float between 0 and 1.
+        * At 0, nothing is plotted.
+        * At 0.5, different things are plotted depending on the value of 'normalise activity time'.
+            * If normalise activity time is True, then half of every polyline is plotted.
+            * If normalise activity time is False, then every polyline will have
+                plotted 50% of the polyline segments of the longest polyline.
+        This facilitates the creation of animations.
+
+    normalise_activity_time: See above.
+
+    proposed_image_size: The height and width of the created image in pixels.
+        The final image size will very slightly from this, but hopefully not
+        too much.
+
+    border_size_px: The size of the border between each individual plot square
+        in pixels.
+
+    line_thickness: The thickness of the line which draws the routes in px.
+    """
+    polylines = []
+    for polyline in encoded_polylines:
+        # If the polyline has nothing in it, just return.
+        polyline = get_polyline_or_none(polyline)
+
+        if polyline is None:
+            continue
+
+        polyline = apply_equirectangular_approximation(polyline)
+        polyline = custom_scale_polyline(polyline)
+        polylines.append(polyline)
+
+    if not polylines:
+        return None
+
+    # To calculate the number of cells in the grid, get the square root of the
+    # number of polylines, then round up. If that number is x, we make an x by
+    # x grid.
+    grid_size = math.ceil(math.sqrt(len(polylines)))
+
+    # Now to calculate the image dimensions, find the largest whole pixel size
+    # square, such that a grid of x by x squares (where x was calculated above)
+    # including borders.
+    square_size = (
+        proposed_image_size - ((grid_size + 1) * border_size_px)
+    ) // grid_size
+    image_size = ((grid_size + 1) * border_size_px) + (grid_size * square_size)
+
+    # Create a white canvas
+    width, height = image_size, image_size
+    image = img.new("RGB", (width, height), "white")
+    draw = ImageDraw.Draw(image)
+
+    for i, polyline in enumerate(polylines):
+        # Calculate col and row from i, then calculate starting position.
+        col = i % grid_size
+        row = i // grid_size
+
+        starting_x = ((col + 1) * border_size_px) + (square_size * col)
+        starting_y = ((row + 1) * border_size_px) + (square_size * row)
+
+        # Scale by the square size
+        polyline = [(x * square_size, y * square_size) for (x, y) in polyline]
+
+        # Translate by the starting position.
+        polyline = [(x + starting_x, y + starting_y) for (x, y) in polyline]
+
+        # Draw the polylines
+        draw.line(
+            polyline[0 : int(polyline_fraction * len(polyline))],
+            fill="black",
+            width=line_thickness,
+        )
+
+    return image
+
+
+def get_polyline_or_none(encoded_polyline: str) -> Polyline | None:
+    decoded_polyline = polyline.decode(encoded_polyline)
+
+    if not decoded_polyline:
+        return None
+
+    return decoded_polyline
+
+
+def apply_equirectangular_approximation(polyline: Polyline) -> Polyline:
+    def equirectangular_approximation(lat, lon, reference_lat):
+        R = 6371  # Radius of the Earth in kilometers
+
+        x = R * lon * math.pi / 180 * math.cos(math.radians(reference_lat))
+        y = R * lat * math.pi / 180
+
+        return x, y
+
+    polyline = [
+        equirectangular_approximation(lat, long, polyline[0][0])
+        for (lat, long) in polyline
+    ]
+
+    return polyline
+
+
+def custom_scale_polyline(polyline: Polyline) -> Polyline:
+    """
+    The goal here is to scale some arbitrary polyline into the center of a unit
+    square. This will make it easier to place them all in the future.
+    """
+    # Before we do any scaling, we are going to negate the y axis. This is
+    # because Pillow treats the top left at 0,0, and not the bottom left.
+    polyline = [(x, -y) for (x, y) in polyline]
+
+    min_x = min(x for (x, y) in polyline)
+    min_y = min(y for (x, y) in polyline)
+    max_x = max(x for (x, y) in polyline)
+    max_y = max(y for (x, y) in polyline)
+
+    # Calculate the dimensions of the bounding box
+    width = max_x - min_x
+    height = max_y - min_y
+
+    # Calculate the scaling factors for x and y to fit the polyline into a unit square
+    if width > height:
+        scaling_factor = 1 / width
+    else:
+        scaling_factor = 1 / height
+
+    translated_polyline = [(x - min_x, y - min_y) for (x, y) in polyline]
+
+    # Calculate the translation offsets to move the scaled polyline to the center of the unit square
+    # translate_x = -min_x * scaling_factor + (1 - width * scaling_factor) / 2
+    # translate_y = -min_y * scaling_factor + (1 - height * scaling_factor) / 2
+
+    # Apply the scaling and translation to each point in the polyline
+    scaled_polyline = [
+        (
+            scaling_factor * x,
+            scaling_factor * y,
+        )
+        for (x, y) in translated_polyline
+    ]
+    return scaled_polyline
+
+
+def translate_polyline(polyline: Polyline, min_canvas_dimension: int) -> Polyline:
+    """
+    At this point, the polyline is centered around (0, 0). We want to center it
+    in the center of the canvas.
+    """
+    return [
+        (x + (min_canvas_dimension / 2), y + (min_canvas_dimension / 2))
+        for (x, y) in polyline
+    ]
+
+
+@dataclasses.dataclass
+class CompactActivity:
+    type: str
+    polyline: str
+    start_date_local: dt.datetime | None
+
+
+def create_gif_image(
+    encoded_polylines: list[str], gif_duration_ms: int, gif_fps: int
+) -> list[Image]:
+    num_frames = int((gif_duration_ms / 1000) * gif_fps)
+
+    return [
+        create_image(encoded_polylines, frame / num_frames)
+        for frame in range(1, num_frames + 1)
+    ]
+
+
+# For testing
+if __name__ == "__main__":
+    from active_statistics.utils import local_storage
+
+    activity_iterator = local_storage.get_activity_iterator(94896104)
+    compact_activities = [
+        CompactActivity(
+            type=activity.type,
+            polyline=activity.map.polyline,
+            start_date_local=activity.start_date_local,
+        )
+        for activity in activity_iterator
+    ]
+    compact_activities.sort(key=lambda x: x.start_date_local)
+
+    activity_types = set(
+        compact_activity.type for compact_activity in compact_activities
+    )
+
+    for activity_type in activity_types:
+        image = create_image(
+            (
+                activity.polyline
+                for activity in compact_activities
+                if activity.type == activity_type
+            ),
+            1.0,
+            True,
+            2000,
+            10,
+            2,
+        )
+
+        if image is None:
+            continue
+
+        image.save(f"{activity_type}_grid.png", "PNG")
+        image.close()
+
+        gif_duration_ms = 2000
+        gif_fps = 40
+        images = create_gif_image(
+            [
+                activity.polyline
+                for activity in compact_activities
+                if activity.type == activity_type
+            ],
+            gif_duration_ms,
+            gif_fps,
+        )
+        images[0].save(
+            f"{activity_type}_grid_animation.gif",
+            save_all=True,
+            append_images=images[1:],
+            duration=gif_duration_ms / gif_fps,
+            loop=0,
+        )
---
+++ backend/active_statistics/statistics/images/polyline_overlay.py
@@ -0,0 +1,207 @@
+import dataclasses
+import math
+from typing import Iterator
+
+import polyline
+from PIL import Image
+from PIL import Image as img
+from PIL import ImageDraw
+from PIL.Image import Image
+
+Polyline = list[tuple[float, float]]
+
+
+def create_image(
+    encoded_polylines: Iterator[str], max_segments_fraction: float = 1.0
+) -> Image | None:
+    """
+    Creates and returns a PIL image object with all polylines overlaid and
+    scaled correctly on top of eachother.
+
+    max_segments_fraction: This is a float between 0 and 1. At 0, nothing is
+    plotted. At 0.5, the maximum number of segments allowed to be drawn is 0.5
+    * the total number of segments in the polyline with the maximimum number
+    of segments. This facilitates the creation of animations.
+    """
+    polylines = []
+    for polyline in encoded_polylines:
+        # If the polyline has nothing in it, just return.
+        polyline = get_polyline_or_none(polyline)
+
+        if polyline is None:
+            continue
+
+        polyline = apply_equirectangular_approximation(polyline)
+        polyline = transform_to_start_and_end_at_zero(polyline)
+        polylines.append(polyline)
+
+    if not polylines:
+        return None
+
+    # Create a white canvas
+    width, height = 2000, 2000
+    image = img.new("RGB", (width, height), "white")
+    draw = ImageDraw.Draw(image)
+
+    max_all_max_chebychev_distances = max(
+        max_chebychev_distance(polyline) for polyline in polylines
+    )
+
+    scaled_polylines = [
+        aesthetically_scale_polyline(
+            polyline, max_all_max_chebychev_distances, min([height, width])
+        )
+        for polyline in polylines
+    ]
+
+    translated_polylines = [
+        translate_polyline(polyline, min([width, height]))
+        for polyline in scaled_polylines
+    ]
+
+    # Get the number of segments in the polyline with the most segments.
+    max_num_polylines = max(len(polyline) for polyline in polylines)
+
+    # Draw the polylines
+    for polyline in translated_polylines:
+        draw.line(
+            polyline[0 : int(max_num_polylines * max_segments_fraction)],
+            fill="black",
+            width=1,
+        )
+
+    return image
+
+
+def get_polyline_or_none(encoded_polyline: str) -> Polyline | None:
+    decoded_polyline = polyline.decode(encoded_polyline)
+
+    if not decoded_polyline:
+        return None
+
+    return decoded_polyline
+
+
+def apply_equirectangular_approximation(polyline: Polyline) -> Polyline:
+    def equirectangular_approximation(lat, lon, reference_lat):
+        R = 6371  # Radius of the Earth in kilometers
+
+        x = R * lon * math.pi / 180 * math.cos(math.radians(reference_lat))
+        y = R * lat * math.pi / 180
+
+        return x, y
+
+    polyline = [
+        equirectangular_approximation(lat, long, polyline[0][0])
+        for (lat, long) in polyline
+    ]
+
+    return polyline
+
+
+def transform_to_start_and_end_at_zero(polyline: Polyline) -> Polyline:
+    return [(x - polyline[0][0], y - polyline[0][1]) for (x, y) in polyline]
+
+
+def max_chebychev_distance(polyline: polyline) -> float:
+    max_x = max(abs(point[0]) for point in polyline)
+    max_y = max(abs(point[1]) for point in polyline)
+    return max([max_x, max_y])
+
+
+def aesthetically_scale_polyline(
+    polyline: Polyline, max_chebychev_distance: float, min_canvas_dimension: float
+) -> Polyline:
+    """
+    Normalizes the polyline so that all points lie between -1 and 1, while starting at
+    Can be multiplied by some scale.
+    """
+
+    # 0.5 is because the max chebychev distance in x means that that distance must fit from the center to the edge, not edge to edge.
+    # 0.95 is because we don't want to hit the edge of the image for aesthetic purposes.
+    polyline = [
+        (
+            x * ((0.95 * 0.5 * min_canvas_dimension) / max_chebychev_distance),
+            y * ((0.95 * 0.5 * min_canvas_dimension) / max_chebychev_distance),
+        )
+        for (x, y) in polyline
+    ]
+
+    # Annoyingly the coordinates of Pillow start at the top left of the screen,
+    # so we need to negate y to have things make sense.
+    polyline = [(x, -y) for (x, y) in polyline]
+    return polyline
+
+
+def translate_polyline(polyline: Polyline, min_canvas_dimension: int) -> Polyline:
+    """
+    At this point, the polyline is centered around (0, 0). We want to center it
+    in the center of the canvas.
+    """
+    return [
+        (x + (min_canvas_dimension / 2), y + (min_canvas_dimension / 2))
+        for (x, y) in polyline
+    ]
+
+
+@dataclasses.dataclass
+class CompactActivity:
+    type: str
+    polyline: str
+
+
+def create_gif_image(
+    encoded_polylines: list[str], gif_duration_ms: int, gif_fps: int
+) -> list[Image]:
+    num_frames = int((gif_duration_ms / 1000) * gif_fps)
+
+    return [
+        create_image(encoded_polylines, frame / num_frames)
+        for frame in range(1, num_frames + 1)
+    ]
+
+
+# For testing
+if __name__ == "__main__":
+    from active_statistics.utils import local_storage
+
+    activity_iterator = local_storage.get_activity_iterator(94896104)
+    compact_activities = [
+        CompactActivity(type=activity.type, polyline=activity.map.polyline)
+        for activity in activity_iterator
+    ]
+    activity_types = set(
+        compact_activity.type for compact_activity in compact_activities
+    )
+
+    for activity_type in activity_types:
+        image = create_image(
+            activity.polyline
+            for activity in compact_activities
+            if activity.type == activity_type
+        )
+
+        if image is None:
+            continue
+
+        image.save(f"{activity_type}.png", "PNG")
+        image.close()
+
+        gif_duration_ms = 3000
+        gif_fps = 20
+        images = create_gif_image(
+            [
+                activity.polyline
+                for activity in compact_activities
+                if activity.type == activity_type
+            ],
+            gif_duration_ms,
+            gif_fps,
+        )
+        images[0].save(
+            f"{activity_type}_animation.gif",
+            save_all=True,
+            append_images=images[1:],
+            duration=gif_duration_ms / gif_fps,
+            loop=0,
+        )
